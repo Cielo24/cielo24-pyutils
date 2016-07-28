36b3483c1fc61b5bdb7cea105feada40df9bba5a
Mattia Procopio <promat85@gmail.com>
2016-07-28 16:37:15 +0200

    Use pip session when installing

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+import pip
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-install_reqs = parse_requirements("requirements/base.txt")
+install_reqs = parse_requirements("requirements/base.txt",
+                                  session=pip.download.PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
 
 setup(
     name='cielo24_utils',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
